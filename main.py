91bf5ae4f145ff51741e1602875eaee02b907026
joekrinke15 <jdk61@duke.edu>
2021-04-14 09:35:21 -0400

    Add main file skeleton

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import requests
+from sklearn.externals import joblib
+import pandas as pd
+
+app = Flask(__name__)
+@app.route('/predict', methods=['POST'])
+def predict():
+     json_ = request.json
+     query_df = pd.DataFrame(json_)
+     query = pd.get_dummies(query_df)
+     prediction = clf.predict(query)
+     return jsonify({'prediction': list(prediction)})
+    
+if __name__ == '__main__':
+     clf = joblib.load('MedCostModel.sav')
+     app.run(port=8080)
