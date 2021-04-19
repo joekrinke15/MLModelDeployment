965c7a5ca9ee4a16345ef739c316d650bda804e6
joekrinke15 <jdk61@duke.edu>
2021-04-19 14:18:35 -0400

    Fix UI to display output instead of broken html template

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,14 +1,29 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import requests
 import pandas as pd
 import pickle
 
 app = Flask(__name__)
+
+def get_dummies(data):
+    categorical_vars = ['sex' , 'smoker', 'region']
+    for var in categorical_vars:
+        one_hot = pd.get_dummies(data[var])
+        data = data.drop(var,axis = 1)
+        data = data.join(one_hot)
+    print(data)
+    return(data)
+    
 def load_model():
     """
-    Loads model
+    Loads model and data.
     """
+    global columns
+    global data
+    
     model = pickle.load(open('MedCostModel.pkl', 'rb'))
+    data = pd.read_csv('MedCosts.csv')
+    columns = data.columns
     return(model)
 
 @app.route("/")
@@ -20,10 +35,31 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-     input = request.json
-     query_df = pd.DataFrame(input, index =[0])
-     prediction = model.predict(query_df)
-     return jsonify({'prediction': list(prediction)})
+    """
+    inputs = request.form.to_dict(flat=False)
+    """
+    input = request.json
+    query_df = pd.DataFrame(input, index =[0])
+    print(query_df)
+    combined_data = pd.concat([query_df, data])
+    combined_dummies = get_dummies(combined_data)
+    
+    input_dummies = combined_dummies.iloc[0]
+    print(input_dummies)
+    prediction = model.predict(input_dummies)
+    return render_template('index.html', prediction_text='Your Predicted Medical Costs Are $ {}'.format(output))
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    """
+    inputs = request.form.to_dict(flat=False)
+    """
+    input = request.json
+    query_df = pd.DataFrame(input, index =[0])
+    prediction = model.predict(query_df)
+    return jsonify({'prediction': list(prediction)})
+
     
 if __name__ == '__main__':
      model = load_model()
--- main.py
+++ main.py
@@ -2,13 +2,29 @@ from flask import Flask, jsonify, request, render_template
 import requests
 import pandas as pd
 import pickle
+import numpy as np
 
 app = Flask(__name__)
+
+def get_dummies(data):
+    categorical_vars = ['sex' , 'smoker', 'region']
+    for var in categorical_vars:
+        one_hot = pd.get_dummies(data[var])
+        data = data.drop(var,axis = 1)
+        data = data.join(one_hot)
+    return(data)
+    
 def load_model():
     """
-    Loads model
+    Loads model and data.
     """
+    global columns
+    global data
+    
     model = pickle.load(open('MedCostModel.pkl', 'rb'))
+    data = pd.read_csv('MedCosts.csv')
+    data = data.drop(columns=['charges'])
+    columns = data.columns
     return(model)
 
 @app.route("/")
@@ -21,12 +37,26 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     """
-    inputs = request.form.to_dict(flat=False)
+    """
+    input = request.form.to_dict(flat=False)
+    query_df = pd.DataFrame(input, index =[0])
+    combined_data = pd.concat([query_df, data])
+    combined_dummies = get_dummies(combined_data)
+    input_dummies = combined_dummies.iloc[0]
+    pred_input = input_dummies.to_numpy().reshape(-1,1).T
+    prediction = np.round(model.predict(pred_input)[0],2)
+    return render_template('prediction.html', prediction_text = 'Your Predicted Medical Costs Are: ${}'.format(prediction))
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    """
     """
     input = request.json
     query_df = pd.DataFrame(input, index =[0])
     prediction = model.predict(query_df)
     return jsonify({'prediction': list(prediction)})
+
     
 if __name__ == '__main__':
      model = load_model()
